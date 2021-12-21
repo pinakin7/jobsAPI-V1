d40f5795a0ca6044caa3be951b970445de0b244d
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-12-21 09:39:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -38,12 +38,7 @@ app.use(cors());
 app.use(xss());
 
 
-
-app.get('/', (req, res) => {
-    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
